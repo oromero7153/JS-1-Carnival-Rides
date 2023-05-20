269e68941aad449672762132da5dce9546287aad
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-05-20 15:03:57 -0700

    ride and ride done

--- index.js
+++ index.js
@@ -13,15 +13,26 @@ let waterIsOn = true
     // green wire is connected
 ride1PowerOn = false
 
+if (greenWireConnected){
+    ride1PowerOn = true
+  }
+  
 //Ride 2 works when 
     //green and yellow wires are connected
 ride2PowerOn = false
+if (greenWireConnected, yellowWireConnected){
+    ride2PowerOn = true
+}
 
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
 ride3PowerOn = false
+if (greenWireConnected, redWireConnected, waterIsOn, !yellowWireConnected){
+    ride3PowerOn = true
+}
+
 
 //Ride 4 has power when:
     //ride wire is connected
